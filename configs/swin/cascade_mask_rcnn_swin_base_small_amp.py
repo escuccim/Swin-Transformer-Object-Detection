9c161e1db2c2426acba9e62b8058eaeacb21c49f
Eric A. Scuccimarra <skooch@gmail.com>
2022-01-26 18:33:04 +0100

    Modifying test pipeline to try to debug error

--- configs/swin/cascade_mask_rcnn_swin_base_small_amp.py
+++ configs/swin/cascade_mask_rcnn_swin_base_small_amp.py
@@ -225,7 +225,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=[(480,  480), (640, 640), (864, 864)],
+        img_scale=[(640, 640), (864, 864)],
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
