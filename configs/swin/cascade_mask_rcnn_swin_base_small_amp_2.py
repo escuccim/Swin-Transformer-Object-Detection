fcfd0df2d88c58b6dcca04cb14ba4591b0d848c4
Eric A. Scuccimarra <skooch@gmail.com>
2022-01-27 15:28:41 +0100

    Update cascade_mask_rcnn_swin_base_small_amp_2.py

--- configs/swin/cascade_mask_rcnn_swin_base_small_amp_2.py
+++ configs/swin/cascade_mask_rcnn_swin_base_small_amp_2.py
@@ -343,7 +343,7 @@ data = dict(train=dict(pipeline=train_pipeline),
 #            val=dict(pipeline=test_pipeline),
            test=dict(pipeline=test_pipeline))
 evaluation = dict(interval=2, metric=['bbox', 'segm'])
-optimizer = dict(_delete_=True, type='AdamW', lr=0.02, betas=(0.9, 0.999), weight_decay=0.05,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.01, betas=(0.9, 0.999), weight_decay=0.05,
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.)}))
