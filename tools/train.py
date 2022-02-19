1e8ef0cb84e3a699d4d681f8c65f71b32742fe24
Eric A. Scuccimarra <skooch@gmail.com>
2022-02-19 06:21:55 +0100

    Commenting out printing of config

--- tools/train.py
+++ tools/train.py
@@ -144,7 +144,7 @@ def main():
     meta['config'] = cfg.pretty_text
     # log some basic info
     logger.info(f'Distributed training: {distributed}')
-    logger.info(f'Config:\n{cfg.pretty_text}')
+#     logger.info(f'Config:\n{cfg.pretty_text}')
 
     # set random seeds
     if args.seed is not None:
