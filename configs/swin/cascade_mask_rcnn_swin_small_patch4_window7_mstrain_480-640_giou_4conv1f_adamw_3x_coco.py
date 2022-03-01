81791b27b738203034a9e68f88d0ab1a6d9d0666
Eric A. Scuccimarra <skooch@gmail.com>
2022-03-01 16:09:25 +0100

    Increasing LR decay

--- configs/swin/cascade_mask_rcnn_swin_small_patch4_window7_mstrain_480-640_giou_4conv1f_adamw_3x_coco.py
+++ configs/swin/cascade_mask_rcnn_swin_small_patch4_window7_mstrain_480-640_giou_4conv1f_adamw_3x_coco.py
@@ -122,7 +122,7 @@ optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), wei
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.)}))
-lr_config = dict(step=[27, 33])
+lr_config = dict(step=[23, 27, 33])
 runner = dict(type='EpochBasedRunnerAmp', max_epochs=36)
 
 # do not use mmdet version fp16
